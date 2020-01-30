e1630744e251ee2071bab8562acf029730e9a484
Chloe Gell <gellert.chloe@gmail.com>
2020-01-30 22:47:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,8 @@ class String
   end
 
   def count_sentences
-    segments = self.split(/!?. {1,}/)
+    # segments = self.split(/!?. {1,}/)
+    setments = self.split(/[!?.] {1,}/)
     if(segments.length == 1)
       return 0 
     else 
